69658cd32f323b9e1a654aef11f462f2c201877a
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-21 21:23:09 +0000

    I made the changes on 2023-09-21 21:23:09

--- _getline.c
+++ _getline.c
@@ -14,41 +14,21 @@ void process_line(char *buffer, unsigned int i)
 
 	ptr = malloc(sizeof(line_t));
 	if (ptr == NULL)
-	{
-		free(ptr);
 		malloc_f();
-	}
 	ptr->n_token = 0;
 	ptr->line = malloc(strlen(buffer) + 1);
 	if (ptr->line == NULL)
-	{
-		free(ptr->line);
 		malloc_f();
-	}
 	strcpy(ptr->line, buffer);
 	linecpy = malloc(sizeof(char) * (strlen(ptr->line) + 1));
 	if (linecpy == NULL)
-	{
-		free(linecpy);
 		malloc_f();
-	}
 	strcpy(linecpy, ptr->line);
 	token = strtok(linecpy, dilim);
-	ptr->tokens = malloc(sizeof(char *) * 3);
-	if (ptr->tokens == NULL)
-	{
-		free(ptr->tokens);
-		malloc_f();
-	}
-	j = 0;
-	while (token != NULL && j < 3)
+	ptr->tokens = malloc(sizeof(char *) * 2);
+	while (token != NULL && j < 2)
 	{
 		ptr->tokens[j] = malloc(sizeof(char) * (strlen(token) + 1));
-		if (ptr->tokens[j] == NULL)
-		{
-			free(ptr->tokens[j]);
-			malloc_f();
-		}
 		strcpy(ptr->tokens[j], token);
 		ptr->n_token++;
 		token = strtok(NULL, dilim);
